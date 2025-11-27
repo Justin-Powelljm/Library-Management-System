33cc2b780b0f2c8a671b7f880e947fab6caebb0e
Justin Powell <powell.oj.justin@gmail.com>
2025-11-27 14:06:47 -0500

    Added initial files to main

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "user-management.h"
+#include "library-management.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    return 0;
+}
---
+++ user-management.h
@@ -0,0 +1,7 @@
+#ifndef user-management_h
+#define user-management_h
+
+#define PASSWORD_LENGTH 64 // To store the hashed value of the password
+#define USERNAME_LENGTH 20
+
+#endif
