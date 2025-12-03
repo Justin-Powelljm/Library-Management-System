92f2a0d231950722b1d57400d30bf6b6f0784d50
nat477-bit <stewartnathaniel20071@gmail.com>
2025-12-03 13:34:53 -0500

    Add library management header file

---
+++ library-management.h
@@ -0,0 +1,43 @@
+#ifndef LIBRARY_MANAGEMENT_H
+#define LIBRARY_MANAGEMENT_H
+
+#define MAXBOOKS 100
+#define MAXLOANS 100
+#define BOOKSFILE "books.dat"
+#define LOANSFILE "loans.dat"
+
+typedef struct
+{
+  char isbn[20];
+  char title[100];
+  char author[50];
+  int year;
+  int quantity;
+  int available;
+} Book;
+
+typedef struct
+{
+  char studentID[20];
+  char isbn[20];
+} Loan;
+
+//Gloabal variables
+extern Book *books[MAXBOOKS];
+extern Loan *loans[MAXLOANS];
+extern int bookCount;
+extern int loanCount;
+
+void loadBooksNloans();
+void saveBooksNloans();
+void AddBook();
+void deleteBook();
+void updateBook();
+void searchBook();
+void listBooks();
+void borrowBook(int stuI);
+void returnBook(int stuI);
+int BookIsLoaned(const char* isbn);
+void adminMenu();
+
+#endif
