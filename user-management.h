53a5c75a6555f6973bd0180406a0c027e7d58f17
nat477-bit <stewartnathaniel20071@gmail.com>
2025-12-04 11:27:08 -0500

    Update user-management.h

--- user-management.h
+++ user-management.h
@@ -1,7 +1,26 @@
 #ifndef user-management_h
 #define user-management_h
 
-#define PASSWORD_LENGTH 64 // To store the hashed value of the password
-#define USERNAME_LENGTH 20
+#define MAXSTUDENTS 50
+
+typedef struct 
+{
+  char firstName[50];
+  char lastName[50];
+  char password[50];
+  int firstLogin;
+  char studentID[20];
+} Student;
+
+void loadStudents();
+void saveStudents();
+void registerStudent();
+int loginStudent();
+void changePassword(int studentIndex)
+void adminChangePassword();
+void forgotPassword();
+void studentMenu(int studentIndex);
+void getTudentID(int studentIndex), char *id);
+int countStudents(int i);
 
 #endif
