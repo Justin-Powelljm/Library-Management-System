16aeac1907aa9a547ccc1a7b0f81135a5cb64ad5
nat477-bit <stewartnathaniel20071@gmail.com>
2025-12-04 11:16:06 -0500

    Update user-management.c

--- user-management.c
+++ user-management.c
@@ -1,54 +1,286 @@
 #include <stdio.h>
 #include <string.h>
-#include "usermanagement.h"
+#include <stdlib.h>
+#include <time.h>
+#include "user-management.h"
+#include "library-management.h"
 
-// SAFE INPUT FUNCTION
-int getIntInput() {
-    int value;
-    while (scanf("%d", &value) != 1) {
-        printf("Invalid input. Enter a number: ");
-        while (getchar() != '\n'); // clear buffer
+//Change from pointer array to object array
+Student students[MAXSTUDENTS];
+int studentCount = 0;
+
+//Generate OTP
+void generateOTP(char *otp)
+{
+    srand(time(NULL));
+    for(int i = 0; i < 6; i++)
+    {
+        otp[i] = '0' + (rand() % 10);
     }
-    return value;
+    otp[6] = '\0';
 }
 
-// ADMIN LOGIN
-void adminLogin() {
-    char username[20], password[20];
+//function to count students
+int countStudents(int i) 
+{
+    if (i >= studentCount) 
+    {
+        return 0;
+    }
+    return 1 + countStudents(i + 1);
+}
 
-    printf("\n===== ADMIN LOGIN =====\n");
-    printf("Username: ");
-    scanf("%s", username);
+void loadStudents() 
+{
+    FILE *fp = fopen("students.dat", "rb");
+    if (fp != NULL) 
+    {
+        fread(&studentCount, sizeof(int), 1, fp);
+        for (int i = 0; i < studentCount; i++) 
+        {
+            fread(&students[i], sizeof(Student), 1, fp);
+        }
+        fclose(fp);
+    } 
+    else 
+    {
+        studentCount = 0;
+    }
+}
 
+void saveStudents() 
+{
+    FILE *fp = fopen("students.dat", "wb");
+    if (fp != NULL) 
+    {
+        fwrite(&studentCount, sizeof(int), 1, fp);
+        for (i = 0; i < studentCount; i++) 
+        {
+            fwrite(&students[i], sizeof(Student), 1, fp);
+        }
+        fclose(fp);
+    }
+}
+
+void registerStudent() 
+{
+    char otp[7];
+    
+    if (studentCount >= MAXSTUDENTS) 
+    {
+        printf("Maximum students reached!\n");
+        return;
+    }
+    
+    printf("\n=== Register ===\n");
+    printf("First Name: ");
+    scanf("%s", students[studentCount].firstName);
+    
+    printf("Last Name: ");
+    scanf("%s", students[studentCount].lastName);
+    
+    sprintf(students[studentCount].studentId, "STU%04d", studentCount + 1);
+    
+    generateOTP(otp);
+    strcpy(students[studentCount].password, otp);
+    students[studentCount].firstLogin = 1;
+    
+    printf("\n*** Registration Complete ***\n");
+    printf("Student ID: %s\n", students[studentCount].studentId);
+    printf("Password: %s\n", otp);
+    printf("Save this!\n");
+    
+    studentCount++;
+    saveStudents();
+}
+
+void forgotPassword() 
+{
+    char first[50], last[50];
+    char newOTP[7];
+    int found = 0;
+    
+    printf("\n=== Forgot Password ===\n");
+    printf("First Name: ");
+    scanf("%s", first);
+    printf("Last Name: ");
+    scanf("%s", last);
+    
+    for (int i = 0; i < studentCount; i++) 
+    {
+        if (strcasecmp(students[i].firstName, first) == 0 &&
+            strcasecmp(students[i].lastName, last) == 0) 
+        {
+            found = 1;
+            
+            generateOTP(newOTP);
+            strcpy(students[i].password, newOTP);
+            students[i].firstLogin = 1;
+            
+            printf("\nNew password: %s\n", newOTP);
+            printf("Change it on next login.\n");
+            
+            saveStudents();
+            return;
+        }
+    }
+    
+    if (!found) 
+    {
+        printf("Student not found!\n");
+    }
+}
+
+int loginStudent() 
+{
+    char id[20], pwd[50];
+    
+    printf("\n=== Student Login ===\n");
+    printf("Student ID: ");
+    scanf("%s", id);
+    
     printf("Password: ");
-    scanf("%s", password);
+    scanf("%s", pwd);
+    
+    for (int i = 0; i < studentCount; i++) 
+    {
+        if (strcmp(students[i].studentId, id) == 0) 
+        {
+            if (strcmp(students[i].password, pwd) == 0) 
+            {
+                if (students[i].firstLogin == 1) 
+                {
+                    printf("\nFirst login. Change your password.\n");
+                    changePassword(i);
+                }
+                return i;
+            } 
+            else 
+            {
+                printf("Wrong password!\n");
+                return -1;
+            }
+        }
+    }
+    
+    printf("Student ID not found!\n");
+    return -1;
+}
 
-    if (strcmp(username, "root") == 0 && strcmp(password, "hello") == 0) {
-        printf("\n✔ Admin logged in successfully!\n");
-        // TODO: call admin menu
-    } else {
-        printf("\n✘ Invalid admin credentials.\n");
+void changePassword(int studentIndex) 
+{
+    char newPass[50], confirm[50];
+    
+    printf("\n=== Change Password ===\n");
+    printf("New password: ");
+    scanf("%s", newPass);
+    
+    printf("Confirm password: ");
+    scanf("%s", confirm);
+    
+    if (strcmp(newPass, confirm) != 0) 
+    {
+        printf("Passwords don't match!\n");
+        return;
+    }
+    
+    strcpy(students[studentIndex].password, newPass);
+    students[studentIndex].firstLogin = 0;
+    
+    printf("Password changed!\n");
+    saveStudents();
+}
+
+void adminChangePassword() 
+{
+    char id[20];
+    char newOTP[7];
+    int found = 0;
+    
+    printf("\n=== Change Student Password ===\n");
+    printf("Student ID: ");
+    scanf("%s", id);
+    
+    for (int i = 0; i < studentCount; i++) 
+    {
+        if (strcmp(students[i].studentId, id) == 0) 
+        {
+            found = 1;
+            
+            generateOTP(newOTP);
+            strcpy(students[i].password, newOTP);
+            students[i].firstLogin = 1;
+            
+            printf("\nPassword reset!\n");
+            printf("New password: %s\n", newOTP);
+            
+            saveStudents();
+            return;
+        }
+    }
+    
+    if (!found) 
+    {
+        printf("Student not found!\n");
     }
 }
 
-// STUDENT LOGIN
-void studentLogin() {
-    printf("\n===== STUDENT LOGIN =====\n");
-    printf("Feature not fully implemented yet.\n");
+void studentMenu(int studentIndex) 
+{
+    int choice;
+    int total;
+    
+    printf("\nWelcome %s %s!\n", 
+           students[studentIndex].firstName, 
+           students[studentIndex].lastName);
+    
+    //use the recursive function
+    total = countStudents(0);
+    printf("Total registered students: %d\n", total);
+    
+    while (1) {
+        printf("\n=== Student Menu ===\n");
+        printf("1. Search Book\n");
+        printf("2. List Books\n");
+        printf("3. Borrow Book\n");
+        printf("4. Return Book\n");
+        printf("5. Change Password\n");
+        printf("6. Logout\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+        
+        if (choice == 1) 
+        {
+            searchBook();
+        } 
+        else if (choice == 2) 
+        {
+            showAllBooks();
+        } 
+        else if (choice == 3) 
+        {
+            borrowBook(studentIndex);
+        } 
+        else if (choice == 4) 
+        {
+            returnBook(studentIndex);
+        } 
+        else if (choice == 5) 
+        {
+            changePassword(studentIndex);
+        } 
+        else if (choice == 6) 
+        {
+            break;
+        } 
+        else 
+        {
+            printf("Invalid choice!\n");
+        }
+    }
 }
 
-// REGISTER STUDENT
-void registerStudent() {
-    printf("\n===== STUDENT REGISTRATION =====\n");
-    printf("Feature not fully implemented yet.\n");
-}
-
-// RESET PASSWORD
-void resetPassword() {
-    printf("\n===== RESET PASSWORD =====\n");
-}
-
-char* hashPassword(const char *plain) {
-    // temporary fake hash
-    return "HASHED123";
+void getStudentId(int studentIndex, char *id) 
+{
+    strcpy(id, students[studentIndex].studentId);
 }
